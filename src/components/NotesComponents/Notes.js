735e4017e6e072b7282cf85b3eaf56381077d54d
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-01 18:11:54 -0400

    creating a notes component responsible for the individual component

---
+++ src/components/NotesComponents/Notes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Note from "./Note";
+
+export default function Notes() {
+
+  return(
+    <div className="notes">
+      <Note />
+
+      <Note />
+
+      <Note />
+
+    </div>
+  );
+}
