78d0b69b9bf63549b8e8fd9e261f931d8d55445b
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-04-01 20:48:37 -0400

    notes.js debugging props

--- src/components/NotesComponents/CreateNote.js
+++ src/components/NotesComponents/CreateNote.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 
-export default function CreateNote() {
+export default function CreateNote({textHandler, saveHandler, inputText}) {
 
   return(
     <div className="note">
       <textarea
         cols = '10'
         rows='5'
+        value = {inputText}
         placeholder="Type..."
+        onChange = {textHandler}
         maxLength={100}
       >
       </textarea>
       <div className="note--footer">
         <span className="label">left</span>
-        <Button variant="outline-light">Save</Button>
+        <Button variant="outline-light" onClick = {saveHandler}>Save</Button>
       </div>
     </div>
   );
--- src/components/NotesComponents/Notes.js
+++ src/components/NotesComponents/Notes.js
@@ -1,24 +1,56 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import Note from "./Note";
 import CreateNote from "./CreateNote";
 import "../css/Note.css";
+import { v4 as uuid } from "uuid";
 
 export default function Notes() {
+
+  //states
+  const [notes, setNotes] = useState([]);
+  const [inputText, setInputText] = useState("");
 
   //get text and store in state
   const textHandler = (e) => {
     setInputText(e.target.value)
   };
 
+  //crud actions
+    //save function
+    const saveHandler =() => {
+      setNotes((prevState) => [
+        ...prevState,
+        {
+          id:uuid(),
+          text: inputText,
+        },
+      ]);
+      //clear the textarea
+      setInputText("");
+    };
+
+    //delete
+    const deleteNote = (id) => {
+      const filteredNotes = notes.filter((note) => note.id !== id);
+      setNotes(filteredNotes);
+    }
+
   return(
     <div className="notes">
-      <Note />
+      {notes.map((note) => (
+      <Note
+        key={note.id}
+        id={note.id}
+        text={note.text}
+        deleteNote={deleteNote}
+      />
+    ))}
 
-      <Note />
-
-      <Note />
-
-      <CreateNote />
+      <CreateNote
+        textHandler={textHandler}
+        saveHandler={saveHandler}
+        inputText={inputText}
+      />
 
     </div>
   );
